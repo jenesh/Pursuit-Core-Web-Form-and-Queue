52e4975bc3d28c27afb2eec5fc288922b21d32c8
Jenesh <jenesh.napit@gmail.com>
2019-08-24 23:48:20 -0400

    progress

--- enableDOMQueue.js
+++ enableDOMQueue.js
@@ -21,14 +21,37 @@ const enableDOMQueue = (queue) => {
     let firstChild = list.firstChild;
     list.removeChild(firstChild)
   }
-}
+}``
 
 document.addEventListener('DOMContentLoaded', () => {
   const enqueueBtn = document.querySelector('#enqueue_btn');
 
   enqueueBtn.addEventListener('click', () => {
-    const 
+    const firstName = document.querySelector('#first-name');
+    const lastName = document.querySelector('#last-name');
+    const address = document.querySelector('#address');
 
+    const size = document.querySelector("input[name='form']:checked");
+    const crust = document.querySelector("#crust");
+    const crustSelected = crust.options[crust.selectedIndex].value;
 
+    let toppingArr = [];
+    const topping = document.querySelectorAll("input[type='checkbox']:checked");
+    for (let x of topping) {
+      toppingArr.push(x.value);
+    }
+
+    const specialInstructions = document.querySelector('textarea');
+    
+    const order = {
+      first_name: firstName.value,
+      last_name: lastName.value,
+      address: address.value,
+      size: size.value,
+      crust: crustSelected,
+      topping: toppingArr,
+      special_instructions: specialInstructions.value
+    }
+    console.log(obj);
   })
 })
